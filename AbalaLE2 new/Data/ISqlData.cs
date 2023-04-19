1b958fd8ad3910d366f1522b15e06f6b9424a1c9
andreaabala <125952588+andreaabala@users.noreply.github.com>
2023-04-19 23:25:13 +0800

    Add files via upload

---
+++ AbalaLE2 new/Data/ISqlData.cs
@@ -0,0 +1,13 @@
+﻿using AbalaLE2_new.Models;
+
+namespace AbalaLE2_new.Data
+{
+    public interface ISqlData
+    {
+        void AddPost(PostModel post);
+        UserModel Authenticate(string username, string password);
+        List<ListPostModel> ListPosts();
+        void Register(string username, string password, string firstName, string lastName);
+        ListPostModel ShowPostDetails(int id);
+    }
+}
---
+++ AbalaLE2 new/Models/UserLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AbalaLE2_new.Models
+{
+    public class UserLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }        
+    }
+
+    
+}
