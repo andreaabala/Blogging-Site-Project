20c76dde26ee50ef429fb9b2dc87dcee1be426af
andreaabala <125952588+andreaabala@users.noreply.github.com>
2023-04-19 18:10:22 +0800

    Add files via upload

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLoggedIn: boolean = false;
+  public redirectUrl: string = "";
+
+  constructor(private http: HttpClient) {
+
+   }
+
+   login (username: string, password: string){
+    return this.http.post<string>("https://localhost:7248/api/login/login", {username, password});
+   }
+}
